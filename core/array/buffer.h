b8cfa4f4233feb43e0e3d6e155bdc2f6f119fb94
Rouchouse Damien <damien.rouchouse@etu.minesparis.psl.eu>
2024-08-16 19:29:45 +0200

    switch from make to cmake

--- core/array/buffer.h
+++ core/array/buffer.h
@@ -1,36 +1,37 @@
 #pragma once
 #include "../utils/types.h"
+#include <iostream>
 
 template <typename T>
 class Buffer
 {
 private:
-    T *ptr_;
-    size_t size_;
+  T *ptr_;
+  size_t size_;
 
 public:
-    Buffer() : ptr_(nullptr), size_(0) {};
-    Buffer(size_t size) : ptr_(new T[size]), size_(size) {};
-    ~Buffer();
-    void allocate(size_t size);
-    T *getPtr() { return ptr_; }
+  Buffer() : ptr_(nullptr), size_(0) {};
+  Buffer(size_t size) : ptr_(new T[size]), size_(size) {};
+  ~Buffer();
+  void allocate(size_t size);
+  T *getPtr() { return ptr_; }
 };
 
 template <typename T>
 Buffer<T>::~Buffer()
 {
-    delete[] ptr_;
-    std::cout << "Buffer deleted" << std::endl;
+  delete[] ptr_;
+  std::cout << "Buffer deleted" << std::endl;
 }
 
 template <typename T>
 void Buffer<T>::allocate(size_t size)
 {
-    if (size < size_)
-    {
-        return;
-    }
-    delete[] ptr_;
-    ptr_ = new T[size];
-    size_ = size;
+  if (size < size_)
+  {
+    return;
+  }
+  delete[] ptr_;
+  ptr_ = new T[size];
+  size_ = size;
 }
