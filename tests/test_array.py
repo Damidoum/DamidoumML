56e090e0bb658108d7bd13d61b2bfd67d579490b
Rouchouse Damien <damien.rouchouse@etu.minesparis.psl.eu>
2024-10-26 16:04:18 +0200

    update python tests

--- tests/test_array.py
+++ tests/test_array.py
@@ -1,8 +1,33 @@
 import pytest
+import damidoumPyML as daml
 
 
-def test_import_damidoumPyML():
-    try:
-        import damidoumPyML  # noqa: F401
-    except ImportError as e:
-        pytest.fail(f"Failed to import damidoumPyML: {e}")
+def test_array_creation():
+    arr_default = daml.Array()
+    assert arr_default.shape == [0]
+    assert arr_default.size == 0
+    assert arr_default.dtype == "float"
+
+    shape = [2, 3]
+    arr_float = daml.Array(1.0, shape)
+    assert arr_float.shape == [2, 3]
+    assert arr_float.size == 6
+    assert arr_float.dtype == "float"
+
+
+def test_zeros():
+    shape = [2, 2]
+    arr_zeros = daml.zeros(shape)
+    assert arr_zeros.shape == [2, 2]
+    assert arr_zeros.size == 4
+
+
+def test_ones():
+    shape = [3, 2]
+    arr_ones = daml.ones(shape)
+    assert arr_ones.shape == [3, 2]
+    assert arr_ones.size == 6
+
+
+if __name__ == "__main__":
+    pytest.main()
